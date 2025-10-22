06d2749350c66ae395d86da170ad552ce243a7b3
Malin Hallgren <malin.m.hallgren@gmail.com>
2025-10-22 16:55:11 +0200

    rebased BasicUser into branch

--- BankApp.cs
+++ BankApp.cs
@@ -4,47 +4,45 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Timers;
-using System.Transactions;
+using BankingApp.Users;
 
 namespace BankingApp
 {
     internal class BankApp
     {
-        //public List<BasicUser> Users;
-        //private List<Transaction> PendingTransactions;
-        private static System.Timers.Timer _transactionTimer = new System.Timers.Timer(1 * 6000);
+        public List<BasicUser> Users;
+        private List<Transaction> PendingTransactions;
+        private static System.Timers.Timer _transactionTimer = new System.Timers.Timer(15 * 60000);
         public static decimal TransactionSum;
 
-        public static void SetTimer()
+
+        /// <summary>
+        /// Uses a hardcoded 15 minute timer to run OnTimedEvent, automatically resets upon completion
+        /// </summary>
+        public static void SetTransactionTimer()
         {
-            _transactionTimer.Elapsed += OnTimedEvent;
+            _transactionTimer.Elapsed += SendPendingTransactions;
             _transactionTimer.AutoReset = true;
             _transactionTimer.Enabled = true;
         }
 
-        private static void OnTimedEvent(Object? source, ElapsedEventArgs time)
-        {
-            SendPendingTransactions(time);
-        }
-
-
         /// <summary>
-        /// Carries out pending transactions
+        /// Events to be run upon completion of the Transaction timer, sends and completes transactions in the 
+        /// PendingTransactions List
         /// </summary>
-        /// <param name="time">ElapsedEventsArgs from the OnTimedEven, contains a time and nothing more</param>
-        private static void SendPendingTransactions(ElapsedEventArgs time)
+        /// <param name="source">The object from which this is sent, in our case, the timer</param>
+        /// <param name="time">Data for the ElapsedEvent from the timer, in this case, just a time</param>
+        private static void SendPendingTransactions(Object? source, ElapsedEventArgs time)
         {
             Console.WriteLine($"Pending transactions have been carried out at {time.SignalTime}");
-            //foreach(var transaction in PendingTransactions)
-            //{
-            //    Console.WriteLine(transaction);
-            //}
-
-        }
-
-        public static void AddTransaction(Transaction transaction)
-        {
-            //TransactionSum += transaction.Amount;
+            foreach (var transaction in PendingTransactions)
+            {
+                Console.WriteLine(transaction);
+                transaction.From.Balance -= transaction.Amount;
+                transaction.To.Balance += transaction.Amount;
+                transaction.Date = time.SignalTime;
+                TransactionSum += transaction.Amount;
+            }
         }
     }
 }

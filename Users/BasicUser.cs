0edacd818ec3aec612a63aab2099d14ff136cb1c
Malin Hallgren <malin.m.hallgren@gmail.com>
2025-10-22 12:45:53 +0200

    removes unneccesary testing properties

--- Users/BasicUser.cs
+++ Users/BasicUser.cs
@@ -9,12 +9,5 @@ namespace BankingApp.Users
     internal class BasicUser
     {
         public static string FilePath { get; private set; } = "BasicUserList.json";
-        public string Name { get; set; }
-        public bool IsAdmin { get; set; }
-
-        public BasicUser(string name)
-        {
-            Name = name;
-        }
     }
 }

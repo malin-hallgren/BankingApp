f7ac883805caf57f2df985dd436e021d0e46d88a
Geovanni Medina Herrera <geovanni.medina.herrera@gmail.com>
2025-10-22 14:29:41 +0200

    BasicUser class
    Introduced a new `BasicUser` class in `BasicUser.cs` with
    properties for user data (`UserName`, `Name`, `PhoneNumber`,
    `EmailAddress`, `Password`, `IsAdmin`).
    
    Added a constructor to initialize these properties.
    
    Included a comment questioning whether the `BasicUser` class
    should be abstract.

--- Users/BasicUser.cs
+++ Users/BasicUser.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace BankingApp.Users
 {
+    // should we make this class abstract?
     internal class BasicUser
     {
+        public string UserName { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string EmailAddress { get; set; }
+        public string Password { get; set; }
+        public bool IsAdmin { get; set; }
+
+        // Constructor
+        public BasicUser(string userName, string name, string phoneNumber, string emailAddress, string password, bool isAdmin)
+        {
+            UserName = userName;
+            Name = name;
+            PhoneNumber = phoneNumber;
+            EmailAddress = emailAddress;
+            Password = password;
+            IsAdmin = isAdmin;
+        }
     }
 }

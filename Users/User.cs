34b2a9dc69fa3f81bb830f3498f343d0573cb29d
Andrés Llano Duran <andres-llano@hotmail.com>
2025-10-22 16:45:54 +0200

    Refactor User class and add placeholders for features
    Refactored the `User` class constructor to include additional
    parameters and call the base class constructor. Added a new
    `OpenAccount` method with minimal implementation. Renamed
    `voidActionLog` to `PrintActionLog` and left it unimplemented.
    
    Commented out unused fields (`logList`, `loanList`) and added
    a TODO block for a potential `RequestLoan` method. Included

--- Users/User.cs
+++ Users/User.cs
@@ -1,4 +1,6 @@
 ﻿using BankingApp.Accounts;
+using System.Net.Mail;
+using System.Xml.Linq;
 
 namespace BankingApp.Users
 {
@@ -6,20 +8,23 @@ namespace BankingApp.Users
     {
         private List<Account>? accountList;
         //private List<Log>? logList;
+
+       // private List<Loan>? loanList;
         public bool IsBlocked { get; set; }
 
         public uint CreditScore { get; set; }
 
-        User(bool isBlocked, uint creditScore)
+        User(string userName, string name, string phoneNumber, string emailAddress, string password, bool isAdmin, bool isBlocked, uint creditScore) : base(userName, name, phoneNumber, emailAddress, password, isAdmin)
         {
             accountList = new List<Account>();
+            //loanList = new LoanList();
             IsBlocked = isBlocked;
             CreditScore = creditScore;
         }
 
-        public void voidActionLog()
+        public void PrintActionLog()
         {
-
+            
         }
 
         public void PrintAccounts()
@@ -29,6 +34,17 @@ namespace BankingApp.Users
                 // TODO: add implementation when Account Class is done.                
             }
 
+        }
+
+      /*  TODO: Implement
+       *  public Loan RequestLoan()
+        {
+            return;
+        }*/
+
+        public Account OpenAccount(string guid, decimal balance, string currency, User owner)
+        {
+            return new Account();
         }
     }
 }

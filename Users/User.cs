9057cad677d7dca13f12136ee179d46c61b27852
Andrés Llano Duran <andres-llano@hotmail.com>
2025-10-22 15:59:19 +0200

    Add User class and System.Text.Json package reference
    Introduced a `User` class in `User.cs` inheriting from `BasicUser`.
    The class includes properties for `IsBlocked` and `CreditScore`,
    a nullable `accountList` field, and methods `voidActionLog()`
    (placeholder) and `PrintAccounts()` (partial implementation).
    Commented out an unused `logList` field.
    
    Added a reference to the `System.Text.Json` package (v9.0.10)
    in `BankingApp.csproj` to enable JSON serialization/deserialization.

---
+++ Users/User.cs
@@ -0,0 +1,34 @@
+﻿using BankingApp.Accounts;
+
+namespace BankingApp.Users
+{
+    internal class User : BasicUser
+    {
+        private List<Account>? accountList;
+        //private List<Log>? logList;
+        public bool IsBlocked { get; set; }
+
+        public uint CreditScore { get; set; }
+
+        User(bool isBlocked, uint creditScore)
+        {
+            accountList = new List<Account>();
+            IsBlocked = isBlocked;
+            CreditScore = creditScore;
+        }
+
+        public void voidActionLog()
+        {
+
+        }
+
+        public void PrintAccounts()
+        {
+            foreach (var a in accountList)
+            {
+                // TODO: add implementation when Account Class is done.                
+            }
+
+        }
+    }
+}

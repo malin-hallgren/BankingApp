47d35873fa9e94991e298316b2f7e503528b7391
Andrés Llano Duran <andres-llano@hotmail.com>
2025-10-22 17:57:17 +0200

    Add Account properties, constructor, and Transfer method
    Introduced new properties (`AccountNumber`, `Balance`, `Currency`, and `Owner`) to the `Account` class to represent account details. Added a constructor to initialize these properties.
    
    Included a `Transfer` method stub for future implementation. Added a `using` directive for the `BankingApp.Users` namespace to enable the use of the `User` class.

--- Accounts/Account.cs
+++ Accounts/Account.cs
@@ -3,10 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BankingApp.Users;
 
 namespace BankingApp.Accounts
 {
     internal class Account
     {
+        public string? AccountNumber { get; set; }
+        public decimal? Balance { get; set; }
+        public string? Currency { get; set; }
+        public User? Owner { get; set; }
+
+        public Account(string accountNumber, decimal balance, string currency, User owner)
+        {
+            string AccountNumber = accountNumber;
+            Balance = balance;
+            Currency = currency;
+            Owner = owner;
+        }
+
+       public void Transfer(User owner, Account account)
+        {
+
+        }
+     
+
+
+
     }
 }

beae9c74664d8ae2a72360eb99cfb52a3558b48f
Andrés Llano Duran <andres-llano@hotmail.com>
2025-10-23 12:46:34 +0200

    Creation and implementations of Saving class

---
+++ Accounts/SavingsAccount.cs
@@ -0,0 +1,25 @@
+﻿using BankingApp.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingApp.Accounts
+{
+    internal class SavingsAccount : Account
+    {
+        public decimal Interest { get; set; }
+
+        public SavingsAccount(string accountNumber, decimal balance, string currency, User owner, decimal interest)
+            : base(accountNumber, balance, currency, owner)
+        {
+            Interest = interest;
+        }
+
+        public override string ToString()
+        {
+            return $"This savings account has an interest rate of: {Interest}";
+        }
+    }
+}

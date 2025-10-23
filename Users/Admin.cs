63dc9c31547f8a2fe7eaf0ff02fff2b691eda9f7
Geovanni Medina Herrera <geovanni.medina.herrera@gmail.com>
2025-10-23 16:53:04 +0200

    Add Admin class with user management functionality
    - Added `ListUsers` method to display user details.
    - Implemented a constructor to initialize `Admin` objects.
    - Added a placeholder `CreateUser` method for future use.
    - Included necessary `using` directives for dependencies.

---
+++ Users/Admin.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingApp.Users
+{
+    internal class Admin : BasicUser
+    {
+
+        // Constructor
+        public Admin(string userName, string name, string phoneNumber, string emailAddress, string password)
+            : base(userName, name, phoneNumber, emailAddress, password, isAdmin: true)
+        {
+        }
+
+        /// <summary>
+        /// Prints a list of all users in the provided list
+        /// </summary>
+        /// <param name="users">Expects a list of BasicUser objects</param>
+        public void ListUsers(List<BasicUser> users)
+        {
+            foreach (var user in users)
+            {
+                if (user.GetType() == typeof(User))
+                {
+                    Console.WriteLine($"User: {user.Name}, Email: {user.EmailAddress}");
+                }
+            }
+        }
+
+        public User CreateUser()
+        {
+
+        }
+    }
+}

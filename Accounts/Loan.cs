c021347f37d4db3ecce5c62dcfa8794f747cf07d
Andrés Llano Duran <andres-llano@hotmail.com>
2025-10-29 00:28:24 +0100

    Implemented Loan class and modified User to
    Add Loan management to User class and new Loan class
    
    The User class now includes a public LoanList property, replacing the previously commented-out loanList field. This change facilitates loan management for users. The User constructor initializes LoanList as an empty list.
    
    Introduced a new Loan class in the BankingApp.Accounts namespace, featuring properties for Size, Interest, and Owner (of type User). The class includes a constructor and an overridden ToString method for loan representation. Necessary using directives have been added for functionality.

---
+++ Accounts/Loan.cs
@@ -0,0 +1,32 @@
+﻿using BankingApp.Users;
+using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingApp.Accounts
+{
+    internal class Loan
+    {
+        public decimal Size { get; set; }
+        public decimal Interest { get; set; }
+        public User Owner { get; set; }
+        public double MortagePercent { get; set; }
+
+        public Loan(User owner, decimal size, decimal interest)
+        {
+            Size = size;
+            Interest = interest;
+            Owner = owner;
+            
+        }
+
+        public override string ToString()
+        {
+            return $"Size of loan: {Size} | Interest: {Interest} | Owner: {Owner} ";
+        }
+    }
+
+}

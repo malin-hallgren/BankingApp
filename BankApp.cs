f9d84289a58592a7c46adbdf9edda5214a439ded
Malin Hallgren <malin.m.hallgren@gmail.com>
2025-10-30 15:00:08 +0100

    adds password masking to entering password for login

--- BankApp.cs
+++ BankApp.cs
@@ -30,40 +30,28 @@ namespace BankingApp
             return new List<BasicUser>(Users);
         }
 
-
-        /// <summary>
-        /// Alternative Get List method that allows for use with both Users and Pending Transactions
-        /// </summary>
-        /// <typeparam name="T">The type of the List you wish to access. Currently supports BasicUser and Transfer</typeparam>
-        /// <returns>A copy of the list corresponding to the passed type</returns>
-        public static List<T> GetList<T>()
-        {
-            if (typeof(T).IsAssignableFrom(typeof(BasicUser)))
-            {
-                var toReturn = new List<BasicUser>(Users);
-                return new List<T>((List<T>)(object)toReturn);
-            }
-
-            else if (typeof(T).IsAssignableFrom(typeof(Transfer)))
-            {
-                var toReturn = new List<Transfer>(PendingTransfer);
-                return new List<T>((List<T>)(object)toReturn);
-            }
-            else
-            {
-                Console.WriteLine("No applicable list found, returning empty, generic list");
-                return new List<T>();
-            }
-        }
-
         /// <summary>
         /// Sets variables at their start state, loads needed files
         /// </summary>
         public static void Startup()
         {
             Users = JsonHelpers.LoadList<BasicUser>(_filePathUsers);
+
+            if(!Users.Exists(x => x.GetType() == typeof(Admin)))
+            {
+                Console.WriteLine("No Admin exists, standard admin user generated. Please set a new password below:");
+                Admin admin = new Admin("Admin", "admin", "587634876538", "admin@redactedbank.se", "dummy");
+                admin.Password = BasicUser.PasswordHash(admin, InputHelpers.ValidString());
+                Users.Add(admin);
+                JsonHelpers.SaveList(Users, _filePathUsers);
+
+                Console.Clear();
+            }
+            
             // We need to save a transaction log, and the sum, and boot them here too
             AsciiHelpers.PrintAscii(AsciiHelpers.LogoPath);
+            Console.ReadLine();
+            Console.Clear();
         }
 
         /// <summary>
@@ -75,37 +63,6 @@ namespace BankingApp
             JsonHelpers.SaveList(Users, _filePathUsers);
             Console.WriteLine("Thank you for using *REDACTED* Bank! We look forward to your next visit!");
             Environment.Exit(0);
-        }
-
-        /// <summary>
-        /// Alternative Add method usable for both PendingTransactions and Users list (and any others we may add)
-        /// </summary>
-        /// <typeparam name="T">The type of object you wish to add, currently supports BasicUser(and subclasses) and Transfer</typeparam>
-        /// <param name="toAdd">The specific object of the aforementioned types you wish to add</param>
-        public static void AddToList<T>(T toAdd)
-        {
-            if(toAdd is BasicUser)
-            {
-                if (toAdd is User)
-                {
-                    var user = toAdd as User;
-                    Users.Add(user);
-                }
-                else
-                {
-                    var admin = toAdd as Admin;
-                    Users.Add(admin);
-                }
-
-                JsonHelpers.SaveList(Users, _filePathUsers);
-                Console.WriteLine($"{toAdd} was added to the Users list");
-            }
-            else if(toAdd is Transfer)
-            {
-                var transfer = toAdd as Transfer;
-                PendingTransfer.Add(transfer);
-                Console.WriteLine($"{toAdd} was added to the Pending Transactions list");
-            }
         }
 
         /// <summary>
@@ -150,6 +107,65 @@ namespace BankingApp
             } //How do we want to display this? Thinking a log for the admin, select the date, and upon selecting the time a list of the transactions?
 
             PendingTransfer.Clear();
+        }
+                       
+
+        [Obsolete("Asked more senior programmer, this is more hassle than it is worth. Use specific getters GetUserList, GetTransferList")]
+        /// <summary>
+        /// Alternative Get List method that allows for use with both Users and Pending Transactions
+        /// </summary>
+        /// <typeparam name="T">The type of the List you wish to access. Currently supports BasicUser and Transfer</typeparam>
+        /// <returns>A copy of the list corresponding to the passed type</returns>
+        public static List<T> GetList<T>()
+        {
+            if (typeof(T).IsAssignableFrom(typeof(BasicUser)))
+            {
+                var toReturn = new List<BasicUser>(Users);
+                return new List<T>((List<T>)(object)toReturn);
+            }
+
+            else if (typeof(T).IsAssignableFrom(typeof(Transfer)))
+            {
+                var toReturn = new List<Transfer>(PendingTransfer);
+                return new List<T>((List<T>)(object)toReturn);
+            }
+            else
+            {
+                Console.WriteLine("No applicable list found, returning empty, generic list");
+                return new List<T>();
+            }
+        }
+
+        [Obsolete("Asked more senior programmer, this is more hassle than it is worth. Use specific setters AddToUserList and AddToTransfer List")]
+        /// <summary>
+        /// Alternative Add method usable for both PendingTransactions and Users list (and any others we may add)
+        /// </summary>
+        /// <typeparam name="T">The type of object you wish to add, currently supports BasicUser(and subclasses) and Transfer</typeparam>
+        /// <param name="toAdd">The specific object of the aforementioned types you wish to add</param>
+        public static void AddToList<T>(T toAdd)
+        {
+            if (toAdd is BasicUser)
+            {
+                if (toAdd is User)
+                {
+                    var user = toAdd as User;
+                    Users.Add(user);
+                }
+                else
+                {
+                    var admin = toAdd as Admin;
+                    Users.Add(admin);
+                }
+
+                JsonHelpers.SaveList(Users, _filePathUsers);
+                Console.WriteLine($"{toAdd} was added to the Users list");
+            }
+            else if (toAdd is Transfer)
+            {
+                var transfer = toAdd as Transfer;
+                PendingTransfer.Add(transfer);
+                Console.WriteLine($"{toAdd} was added to the Pending Transactions list");
+            }
         }
     }
 }

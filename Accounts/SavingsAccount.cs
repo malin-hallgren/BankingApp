34442e6d4ca2ab0726e254fb856c54073f204668
Geovanni Medina Herrera <geovanni.medina.herrera@gmail.com>
2025-10-30 22:46:21 +0100

    Add properties and update SavingsAccount constructor
    Updated the `SavingsAccount` class to include `get` and `set`
    properties for `Balance` and `Interest`, enabling encapsulated
    access. Modified the constructor to set these properties and
    changed the default `interest` value to `2`. Added commented-out
    placeholders for `getInterest` and `setInterest` methods.
    
    Updated `Program.cs` with commented-out examples for creating
    a `User` and `SavingsAccount` and demonstrating balance
    operations. No functional changes were made to `Program.cs`.

--- Accounts/SavingsAccount.cs
+++ Accounts/SavingsAccount.cs
@@ -9,17 +9,39 @@ namespace BankingApp.Accounts
 {
     internal class SavingsAccount : Account
     {
-        public decimal Interest { get; set; }
+        //DONE:
+        // skapa en get set för balance och sätt den från konstruktorn 
+        // desmma för currency
+        
+        // TODO:
+        // add to action log - när ett account skapas. 
 
-        public SavingsAccount(string currency, User owner, decimal interest = 0, decimal balance = 0)
+
+        public decimal Interest { get; set; }
+        public decimal Balance { get; set; }
+
+
+        public SavingsAccount(string currency, User owner, decimal interest = 2, decimal balance = 0)
             : base(currency, owner, balance)
         {
-            Interest = interest;
+            this.interest = interest;
+            this.balance = balance;
+            
         }
 
+        // public decimal getInterest(){
+        //     return interest;
+        // }
+
+        // public decimal setInterest(decimal interest){
+        //     this.interest = interest;
+        // }
+
+
+        
         public override string ToString()
         {
-            return $"This savings account has an interest rate of: {Interest}";
+           return $"This savings account has an interest rate of: {Interest}";
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -13,6 +13,13 @@ namespace BankingApp
             Console.ReadLine();
             
             BankApp.Exit();
+
+            // User user1 = new User("JD", "John", "03555445", "johndoe@gmail.com", "Doepasssword", false, 152);
+            // SavingsAccount savingsAccount1 = new SavingsAccount("USD", user1, 3.5m);
+            // decimal balance = savingsAccount1.getBalance();
+            // Console.WriteLine("balance: " + balance);
+            // savingsAccount.setBalance(10000000);
+            // Console.WriteLine("balance: " + balance);
         }
     }
 }

267d1c8b965bab06632101cdde09cc6971400c81
gronis-94@hotmail.com <gronis-94@hotmail.com>
2025-10-30 14:41:52 +0100

    Added UI element for admin and customer

---
+++ UI/CustomerUI.cs
@@ -0,0 +1,51 @@
+﻿using BankingApp.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingApp.Menus
+{
+    internal class CustomerUI
+    {
+        // This is called with CustomerUI.Start
+        public static void Start()
+        {
+            string prompt = "Customer Menu"; // Might be cool if this íncludes "user.name" or something
+            string[] options =
+            {
+                "Print Action Log",
+                "Print Accounts",
+                "Request Loan",
+                "Open Account"
+            };
+
+            Menu menu = new Menu(prompt, options);
+            int selectedIndex = menu.Run();
+
+            switch (selectedIndex)
+            {
+                case 0:
+                    // Print Action Log
+                    break;
+
+                case 1:
+                    // Print Accounts
+                    break;
+
+                case 2:
+                    // Request Loan
+                    break;
+
+                case 3:
+                    // Open Account
+                    break;
+
+                default:
+                    // If error somehow
+                    break;
+            }
+        }
+    }
+}

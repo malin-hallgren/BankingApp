ebd8023dc1416529427dec97d75c3b1c2df5180d
Malin Hallgren <malin.m.hallgren@gmail.com>
2025-10-30 16:06:28 +0100

    Populates menus with options, app now functioning

--- UI/CustomerUI.cs
+++ UI/CustomerUI.cs
@@ -1,24 +1,26 @@
 ﻿using BankingApp.Utilities;
+using BankingApp.Users;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BankingApp.Menus
+namespace BankingApp.UI
 {
     internal class CustomerUI
     {
         // This is called with CustomerUI.Start
-        public static void Start()
+        public static void Start(User user)
         {
-            string prompt = "Customer Menu"; // Might be cool if this íncludes "user.name" or something
+            string prompt = "Customer Menu " + user.Name; // Might be cool if this íncludes "user.name" or something. It absolutely can, long as we send the User object along!
             string[] options =
             {
                 "Print Action Log",
                 "Print Accounts",
                 "Request Loan",
-                "Open Account"
+                "Open Account",
+                "Log out"
             };
 
             Menu menu = new Menu(prompt, options);
@@ -27,20 +29,42 @@ namespace BankingApp.Menus
             switch (selectedIndex)
             {
                 case 0:
+                    Console.Clear();
+                    //OutputHelpers.PrintList(user.logList);
+                    Console.WriteLine("Action List - Coming Soon");
+                    Console.ReadLine();
+                    Start(user);
                     // Print Action Log
                     break;
 
                 case 1:
+                    Console.Clear();
+                    user.PrintAccounts();
+                    Console.WriteLine("Account List - Coming Soon");
+                    Console.ReadLine();
+                    Start(user);
                     // Print Accounts
                     break;
 
                 case 2:
+                    Console.Clear();
+                    Console.WriteLine("Request Loan - Coming Soon");
+                    Console.ReadLine();
+                    Start(user);
                     // Request Loan
                     break;
 
                 case 3:
+                    Console.Clear();
+                    Console.WriteLine("Open Account - Coming Soon");
+                    Console.ReadLine();
+                    Start(user);
                     // Open Account
                     break;
+                case 4:
+                    Console.Clear();
+                    Console.WriteLine("Logged out successfully. Please log in again");
+                    return;
 
                 default:
                     // If error somehow
---
+++ UI/Login.cs
@@ -0,0 +1,43 @@
+﻿using BankingApp.Users;
+using BankingApp.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingApp.UI
+{
+    internal class Login
+    {
+        public static (BasicUser?, bool) Start()
+        {
+            string prompt = "Login Menu";
+            string[] options =
+            {
+                "Log in",
+                "Quit"
+            };
+
+            Menu menu = new Menu(prompt, options);
+            int selectedIndex = menu.Run();
+
+            switch (selectedIndex)
+            {
+                case 0:
+                    Console.Clear();
+                    (BasicUser?, bool) user = BasicUser.LogInCheck();
+                    Console.Clear();
+                    return user;
+                case 1:
+                    Console.Clear();
+                    BankApp.Exit();
+                    return (null, false);
+                default:
+                    Console.Clear();
+                    BankApp.Exit();
+                    return (null, false);
+            }
+        }
+    }
+}
